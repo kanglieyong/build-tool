0dae88e96b75afcb32a8324ac08f32bba6c42b0c
kanglieyong <kanglieyong@gmail.com>
2018-01-27 02:40:44 +0800

    srcs

---
+++ bazel-project/java-project/pl/src/main/java/org/pl/Greeting.java
@@ -0,0 +1,8 @@
+package org.pl;
+
+public class Greeting
+{
+    public static void sayHi() {
+        System.out.println("Hi!");
+    }
+}
---
+++ bazel-project/java-project/pl/src/main/java/org/pl/PengMain.java
@@ -0,0 +1,8 @@
+package org.pl;
+
+public class PengMain
+{
+    public static void main(String args[]) {
+        Greeting.sayHi();
+    }
+}
